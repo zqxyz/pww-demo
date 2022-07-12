2613392289c8ff85d7efbc9bed6a3f62f8b239ba
Zach <code@zquint.xyz>
2022-07-12 11:57:54 -0400

    corrected css calc issue causing build process to transpile incorrectly

--- src/components/back-to-top/BackToTop.js
+++ src/components/back-to-top/BackToTop.js
@@ -1,4 +1,5 @@
 import Container from "../container/Container"
+// css inside main.css
 
 const BackToTop = () => {
   return (
@@ -9,8 +10,8 @@ const BackToTop = () => {
           id="back-to-top"
           onClick={window.scrollTo(0, 0)}
         >
-          <div id="scroll-up-arrow">
-            <div className="vertical-center">
+          <div id="scroll-up-arrow-container">
+            <div id="scroll-up-arrow">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-up" viewBox="0 0 16 16">
                 <path fillRule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z" />
               </svg>
